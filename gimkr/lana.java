5a0600dec98eacd4835f715b4548099f5ab08198
harli17 <ana17.jagodic@gmail.com>
2019-11-11 13:46:20 +0000

    Prva naloga

---
+++ gimkr/lana.java
@@ -0,0 +1,14 @@
+package gimkr;
+
+import java.util.*;
+public class lana{
+    public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+
+    int spremenljivka = 5;
+    int spremenljivka2 = 8;
+    int vsota = spremenljivka + spremenljivka2;
+    System.out.println(vsota);
+
+    }
+}
